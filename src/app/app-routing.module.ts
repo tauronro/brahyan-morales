3366d2df8febfd3b280a88ffb9df81be39f2c5a7
Brahyan Morales <bgustavommorales@gmail.com>
2021-05-12 14:50:39 -0500

    hoja de vida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BodyComponent } from './home/body/body.component';
+import { ContacComponent } from './home/contac/contac.component';
+import { InfoComponent } from './home/info/info.component';
+import { StudiesComponent } from './home/studies/studies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: BodyComponent,
+      },
+      {
+        path: 'contacto',
+        component: ContacComponent,
+      },
+      {
+        path: 'informacion',
+        component: InfoComponent,
+      },
+      {
+        path: 'estudios',
+        component: StudiesComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BodyComponent } from './home/body/body.component';
+import { ContacComponent } from './home/contac/contac.component';
+import { InfoComponent } from './home/info/info.component';
+import { StudiesComponent } from './home/studies/studies.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BodyComponent,
+    ContacComponent,
+    InfoComponent,
+    StudiesComponent,
+    
   ],
   imports: [
     BrowserModule,
